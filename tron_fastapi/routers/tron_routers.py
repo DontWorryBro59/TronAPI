21c13b527277456e01aede0e8404dd1e920c6575
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 20:58:12 +0500

    Added pydantic schemas for typing

--- tron_fastapi/routers/tron_routers.py
+++ tron_fastapi/routers/tron_routers.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from tron_fastapi.config.config import logger
-from tron_fastapi.repositories import TronRepo, TronDB
 from tron_fastapi.database.db_helper import db_help
 from tron_fastapi.models.tables import AddressRequestORM
+from tron_fastapi.repositories import TronRepo, TronDB
+from tron_fastapi.schemas.TronSchemas import WalletBase
 
 tn_router = APIRouter(tags=["Tron"], prefix="/tron")
 
@@ -29,5 +30,6 @@ async def get_requests(
     page: int = 1,
     page_size: int = 10,
     session: AsyncSession = Depends(db_help.get_session),
-):
+) -> list[WalletBase]:
     result = await TronDB.get_last_data(page=page, page_size=page_size, session=session)
+    return result
