c506c85a23ce249ed7f876dc78b7d6f22840d75f
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 18:05:44 +0500

    Added BASE_DIR for url's path

--- tron_fastapi/config/config.py
+++ tron_fastapi/config/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings
 import logging
 
@@ -7,12 +8,12 @@ logging.basicConfig(level=logging.INFO,
 
 logger = logging.getLogger(__name__)
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite+aiosqlite:///./db.sqlite3"
+    DATABASE_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     #DB_ECHO: bool = False
     DB_ECHO: bool = True
-
 
 settings = Settings()
 
