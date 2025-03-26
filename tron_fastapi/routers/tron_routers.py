b6c9265885597ddb5a7d9cdf86ba0f830e9d83b2
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 23:01:36 +0500

    change tron_routers

--- tron_fastapi/routers/tron_routers.py
+++ tron_fastapi/routers/tron_routers.py
@@ -1,17 +1,20 @@
+from typing import List, Union
+
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from schemas.TronSchemas import WalletBase
 from tron_fastapi.config.config import logger
 from tron_fastapi.database.db_helper import db_help
 from tron_fastapi.models.tables import AddressRequestORM
 from tron_fastapi.repositories import TronRepo, TronDB
-from tron_fastapi.schemas.TronSchemas import WalletBase
+from tron_fastapi.schemas.TronSchemas import WalletFromDB, WalletCreate
 
 tn_router = APIRouter(tags=["Tron"], prefix="/tron")
 
 
 @tn_router.post("/{address}")
-async def check_adress(
+async def check_address(
     address: str, session: AsyncSession = Depends(db_help.get_session)
 ):
     if not TronRepo.check_address(address):
@@ -22,6 +25,7 @@ async def check_adress(
     new_wallet = AddressRequestORM(address=address, **result)
     ##Записываем данные в БД и получаем ответ
     await TronDB.post_new_wallet(wallet=new_wallet, session=session)
+    result = WalletCreate.parse_obj(result)
     return result
 
 
@@ -30,6 +34,6 @@ async def get_requests(
     page: int = 1,
     page_size: int = 10,
     session: AsyncSession = Depends(db_help.get_session),
-) -> list[WalletBase]:
+) -> List[WalletFromDB]:
     result = await TronDB.get_last_data(page=page, page_size=page_size, session=session)
     return result
