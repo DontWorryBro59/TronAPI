b2522c50a2caf7e1b599a92f8f9958945a8bf94f
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 15:33:48 +0500

    Planning endpoints

--- tron_fastapi/main.py
+++ tron_fastapi/main.py
@@ -1,13 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from tronpy import Tron
 app = FastAPI()
+tron = Tron()
 
+@app.post('/tron/{address}')
+async def root(address:str):
+    # Здесь нужно реализовать логику получения информации о кошельке
+    # и записи ее в базу данных
+    pass
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
-
+@app.get('/tron')
+async def get_requests(page: int = 1, page_size: int = 10):
+    # Здесь нужно реализовать логику получения списка запросов
+    pass
 
 if __name__ == '__main__':
     uvicorn.run('main:app')
