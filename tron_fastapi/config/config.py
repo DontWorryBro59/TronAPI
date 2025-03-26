c6555b664db4b447c67631035bf2792086a7f4e0
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 16:38:06 +0500

    Added logger

--- tron_fastapi/config/config.py
+++ tron_fastapi/config/config.py
@@ -1,4 +1,11 @@
 from pydantic_settings import BaseSettings
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
 
 
 class Settings(BaseSettings):
