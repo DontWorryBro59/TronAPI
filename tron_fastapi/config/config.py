97ae38ae56fdabb41a98cd74409088a47972b798
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-26 20:31:14 +0500

    Added default api keys

--- tron_fastapi/config/config.py
+++ tron_fastapi/config/config.py
@@ -19,8 +19,11 @@ class Settings(BaseSettings):
     """
 
     DATABASE_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    # DB_ECHO: bool = False
-    DB_ECHO: bool = True
+    DB_ECHO: bool = False
+    # DB_ECHO: bool = True
+    API_KEY1: str = "d0be5201-e79d-48e9-853f-88fc2b3106d9"
+    API_KEY2: str = "18ac5f55-bba2-4beb-a1bf-658564f8778a"
+    API_KEY3: str = "b9815890-3363-4b80-8f3a-2b5499d8876a"
 
 
 settings = Settings()
